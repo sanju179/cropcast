7a33886dad11bc1b2f1ace54156aee7400e04238
sanju179 <sanjeevinir17@outlook.com>
2024-03-28 14:01:05 +0530

    weather.py now prints one value for current temperature and humidity. Latitude and Longitude can now be fetched from some address, say "Hyderabad", using geopy.

--- weather.py
+++ weather.py
@@ -3,31 +3,52 @@ import json
 from geopy.geocoders import Nominatim
 import geocoder
 
-g = geocoder.ip('me')
-print(g.latlng)
-lat = g.latlng[0]
-lng = g.latlng[1] 
+lat = 17.4065
+lng = 78.4772
 
-geoloc = Nominatim(user_agent="GetLoc")
+loc = Nominatim(user_agent="Geopy Library")
 
-location = geoloc.reverse(f"{lat}, {lng}")
-print(f"Location: {location.address}")
-# sending request to api
-api_url = f"https://api.tomorrow.io/v4/weather/forecast?location={lat}%2C%20{lng}&timesteps=1d&apikey=ANFakkplK6SMxJ3qANjzW6y5dAxeWi9V"
-headers = {"accept": "application/json"}
-# getting response
-response = requests.get(api_url,headers=headers)
-data = response.json()
+def address_to_latlng(place):
+    # entering the location name
+    getLoc = loc.geocode(place)
 
-dic = {}
+    # printing address
+    print(getLoc.address)
+    return getLoc
 
-daily_data = data['timelines']['daily']
-for day in daily_data:
+geoLoc = address_to_latlng("Hyderabad")
+
+# printing latitude and longitude
+print("Latitude = ", geoLoc.latitude, "\n")
+print("Longitude = ", geoLoc.longitude)
+
+
+
+
+def fetch_temp(lat, lng):
+    dic = {}
+    # sending request to api
+    url = "https://api.tomorrow.io/v4/weather/realtime?location=hyderabad%20&apikey=ANFakkplK6SMxJ3qANjzW6y5dAxeWi9V"
+
+    headers = {"accept": "application/json"}
+    
+    response = requests.get(url, headers=headers)
+
+
+    data = response.json()
+    
+    day = data['data']
     time = day['time']
-    temperature = day['values']['temperatureApparentAvg']
-    humidity = day['values']['humidityAvg']
-    # wind_speed = day['values']['rainfallAvg']
-    
+    temperature = day['values']['temperature']
+    humidity = day['values']['humidity']
+        # wind_speed = day['values']['rainfallAvg']
+        
     dic[time] = [temperature, humidity]
-    
-print(dic)
+    return dic
+
+weather = fetch_temp(geoLoc.latitude, geoLoc.longitude)
+print(weather)
+key = list(weather.keys())[0]
+print(f"Time: {key}")
+print(f"Current Temperature: {weather[key][0]}")
+print(f"Humidity: {weather[key][1]}")
